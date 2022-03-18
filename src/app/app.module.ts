d6c89da315362a0a0e5d2fa68b5fdf8fccb08d0b
antonialopes <euranlopes@hotmail.com>
2022-03-18 05:44:09 -0300

    subindo LH angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
+import { AppRountingModule } from './app-rounting/app-rounting.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { MuralVagasComponent } from './mural-vagas/mural-vagas.component';
+import { RouterModule } from '@angular/router';
+import { PainelVagasComponent } from './painel-vagas/painel-vagas.component';
+import { MenuComponent } from './menu/menu.component';
+import { RodapeComponent } from './rodape/rodape.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MuralVagasComponent,
+    PainelVagasComponent,
+    MenuComponent,
+    RodapeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule,
+    AppRountingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
